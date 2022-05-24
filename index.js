700013b99e17a5cbf82725d64fb08eb792f71f45
Jahid Hassan <jahidhassan.programmer@gmail.com>
2022-05-24 22:50:47 +0600

    stripe payment complete

--- index.js
+++ index.js
@@ -103,9 +103,8 @@ async function run() {
 
 
         //Stripe Payments
-        app.post('/create-payment-intent/', async (req, res) => {
+        app.post('/create-payment-intent', async (req, res) => {
             const price = req?.body.amount;
-            console.log(req?.body);
             const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
@@ -114,6 +113,21 @@ async function run() {
             });
             res.send({ clientSecret: paymentIntent.client_secret })
         });
+
+        //Put Method Starts here
+        app.put('/peymentStatus/:id', async (req, res) => {
+            const id = req?.params.id;
+            const query = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    'isPaid': true
+                }
+            }
+            const result = await ordersCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+        })
+
 
 
         //Delete method Starts Here
