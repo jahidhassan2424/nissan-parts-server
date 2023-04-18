4010d536d42e6660bc7e6162ade68ac20bd42f9e
Jahid Hassan <jahidhassan.programmer@gmail.com>
2023-04-18 00:45:02 -0300

    maybe solved

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const corsConfig = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
-
+// 
 app.use(cors(corsConfig))
 app.options("*", cors(corsConfig))
 app.use(function (req, res, next) {
